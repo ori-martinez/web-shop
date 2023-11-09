e86b9d18cf63e212cba36c3b3d160fdda335a80d
Oriana E. Martínez <martinez.oriana.e@gmail.com>
2023-11-08 23:36:23 -0400

    [UPDATE] Navbar - PageLayout

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -29,35 +29,39 @@ export const Navbar = () => {
 
     // RETORNO
     return (<>
-        <nav className='px-48 pt-4 pb-20 -mx-40 bg-pink-baby rounded-b-full'>
-            <div className='flex flex-col'>
-                <div className='w-full grid grid-cols-3'>
-                    <div className='col-span-2 md:col-span-1 md:col-start-2 lg:col-start-2'>
-                        <InputSearch />
-                    </div>
+        <div className="-mt-[200px] overflow-x-hidden">
+            <div className="ml-[-50%] w-[200%] h-[500px] rounded-b-[100%] bg-pink-baby">
+                <nav className='pt-56 flex items-center justify-center'>
+                    <div className='w-[48%] flex flex-col'>
+                        <div className='w-full grid grid-cols-3'>
+                            <div className='col-span-2 md:col-span-1 md:col-start-2 lg:col-start-2'>
+                                <InputSearch />
+                            </div>
 
-                    <div className='flex justify-end'>
-                        <TextButton icon={<BiUserCircle className='w-5 h-5' />} text='Iniciar sesión' />
-                    </div>
-                </div>
-                <div className='flex justify-around flex-wrap'>
-                    <img alt='Logo' className='w-28 h-auto md:w-36' src='https://static.whataform.com/avatar/avatar_d61bce3bef7ff7b_5520ec23cf2e8e0492339cd1f521b67fc1fb4b29.webp' title='Visitar mi perfil'/>
+                            <div className='flex justify-center'>
+                                <TextButton icon={<BiUserCircle className='w-5 h-5' />} text='Iniciar sesión' />
+                            </div>
+                        </div>
+                        <div className='mx-12 flex justify-around flex-wrap md:mx-24 lg:mx-40'>
+                            <img alt='Logo' className='w-28 h-auto md:w-36' src='https://static.whataform.com/avatar/avatar_d61bce3bef7ff7b_5520ec23cf2e8e0492339cd1f521b67fc1fb4b29.webp' title='Visitar mi perfil'/>
 
-                    <div className='mt-4 md:mt-8 flex flex-col'>
-                        <h1 className='text-gray-700 font-bold md:text-xl'>AHEGAO</h1>
-                            
-                        <p className='text-sm text-gray-500 md:text-base'>Tienda virtual | Caracas / La Guaira - Vzla <sub>VE</sub></p>
-                    </div>
+                            <div className='mt-4 md:mt-8 flex flex-col'>
+                                <h1 className='text-gray-700 font-bold md:text-xl'>AHEGAO</h1>
+                                    
+                                <p className='text-sm text-gray-500 md:text-base'>Tienda virtual | Caracas / La Guaira - Vzla <sub>VE</sub></p>
+                            </div>
 
-                    <Link className='mt-8 md:mt-12' target="_blank" title='Instagram' to='https://www.instagram.com/ahegaove'>
-                        <BiLogoInstagram className='w-6 h-6 text-gray-700'/>
-                    </Link>
-                </div>
+                            <Link className='mt-8 md:mt-12' target="_blank" title='Instagram' to='https://www.instagram.com/ahegaove'>
+                                <BiLogoInstagram className='w-6 h-6 text-gray-700'/>
+                            </Link>
+                        </div>
+                    </div>
+                </nav>
             </div>
-        </nav>
+        </div>
 
         {showScrollMenu && (
-            <nav className='py-2 w-full fixed top-0 grid grid-cols-4 place-items-center bg-white shadow-md'>
+            <nav className='py-2 w-full fixed top-0 z-10 grid grid-cols-4 place-items-center bg-white shadow-md'>
                 <img alt='Logo' className='w-8 md:w-12 h-auto' src='https://static.whataform.com/avatar/avatar_d61bce3bef7ff7b_5520ec23cf2e8e0492339cd1f521b67fc1fb4b29.webp'/>
 
                 <div className='col-span-2'>
--- src/pages/PageLayout.js
+++ src/pages/PageLayout.js
@@ -1,53 +1,12 @@
 /* Components */
 import { Navbar } from '../components/Navbar';
-import { ProductModal } from '../components/modals/ProductModal';
-/* Hooks */
-import { useState } from 'react';
 
 // PLANTILLA
 /* Estructura Base de una Página */
-export const PageLayout = ({children}) => {
-    // CONSTANTES
-    const [open, setOpen] = useState(false);
+export const PageLayout = ({ children }) => (
+    <div className='min-h-screen bg-pink-strong'>
+        <Navbar/>
 
-    /* Funcionalidad de Apertura del Modal */
-    const handleOpenModals = () => {
-        setOpen(true);
-    }
-
-    // RETORNO
-    return (
-        <div className='min-h-screen bg-pink-strong overflow-x-hidden'>
-            <Navbar/>
-
-            <div className='p-2 mx-2 -mt-16 bg-white rounded-xl shadow-2xl lg:mx-28 lg:-mt-12'>
-                <button
-                    onClick={() => setOpen(!open)}
-                >
-                    Modal
-                </button>
-
-                <ProductModal
-                    open={open}
-                    setOpen={setOpen}
-                    title={'Arnés de Glúteos con Esposas'}
-                    description={'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text'}
-                    collageImages={[
-                        'https://res.cloudinary.com/djosi3wtf/image/upload/v1699462827/imagen_1_Kit_BDSM_Set_ptvzad.jpg',
-                        'https://res.cloudinary.com/djosi3wtf/image/upload/v1699462827/imagen_2_Kit_BDSM_Set_s6wo2n.jpg'
-                    ]}
-                    colors={[
-                        'Negro',
-                        'Rojo'
-                    ]}
-                />
-
-
-                { children }
-            </div>
-        </div>
-    )
-}
-
-
-
+        <div className='p-2 mx-2 bg-white rounded-xl shadow-2xl lg:mx-28 lg:-mt-12'>{ children }</div>
+    </div>
+);

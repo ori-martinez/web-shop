22ffdeace9314809ccced83e263820e8c0b21e3d
Luis Salazar <41168258+nightstormy@users.noreply.github.com>
2023-11-10 15:07:08 -0400

    Update PageLayout.js

--- src/pages/PageLayout.js
+++ src/pages/PageLayout.js
@@ -1,24 +1,24 @@
 /* Components */
-import { CartDescriptionSection } from '../components/sections/CartDescriptionSection';
-import { Footer } from '../components/Footer';
-import { Navbar } from '../components/Navbar';
+import {CartDescriptionSection} from '../components/sections/CartDescriptionSection';
+import {Footer} from '../components/Footer';
+import {Navbar} from '../components/Navbar';
 /* Icons */
-import { HiArrowUp } from 'react-icons/hi';
+import {HiArrowUp} from 'react-icons/hi';
 /* Hooks */
-import { useEffect, useState } from 'react';
+import {useEffect, useState} from 'react';
 /* React Scroll */
-import { animateScroll } from 'react-scroll';
+import {animateScroll} from 'react-scroll';
 
 // FUNCIONES
 /* Funcionalidad de Scroll hacia Arriba */
-const scrollTop = () =>  animateScroll.scrollToTop();
+const scrollTop = () => animateScroll.scrollToTop();
 
 // PLANTILLA
 /* Estructura Base de una Página */
-export const PageLayout = ({ children, hasCartDescription = true }) => {
+export const PageLayout = ({children, hasCartDescription = true}) => {
     // CONSTANTES
     /* Estado de Muestra del Botón */
-    const [ showButton, setShowButton ] = useState(false);
+    const [showButton, setShowButton] = useState(false);
 
     // FUNCIONES
     /* Obtención del Botón Fijo */
@@ -39,23 +39,32 @@ export const PageLayout = ({ children, hasCartDescription = true }) => {
 
     // RETORNO
     return (
-    <div className='min-h-screen bg-pink-strong'>
-        <Navbar/>
+        <div
+            className='min-h-screen'
+            style={{
+                background: 'url("https://res.cloudinary.com/djosi3wtf/image/upload/v1699642007/RF_q09uyo.png")',
+                backgroundPosition: 'center',
+                backgroundRepeat: 'repeat', // Cambiado de 'no-repeat' a 'repeat'
+                backgroundSize: 'auto', // Cambiado de 'cover' a 'auto'
+                backgroundAttachment: 'fixed', // Cambiado de 'scroll' a 'fixed'
+            }}
+        >
+            <Navbar />
 
-        <div className='p-2 mx-2 -mt-12 bg-white rounded-2xl shadow-2xl lg:mx-72 md:-mt-12'>{ children }</div>
+            <div className='p-2 mx-2 -mt-12 bg-white rounded-2xl shadow-2xl lg:mx-72 md:-mt-12'>{children}</div>
 
-        {hasCartDescription && (<CartDescriptionSection onClick={scrollTop} />)}
+            {hasCartDescription && <CartDescriptionSection onClick={scrollTop} />}
 
-        <Footer />
+            <Footer />
 
-        {showButton && (
-            <button
-                className='p-1.5 fixed right-6 bottom-20 bg-gray-500 rounded-lg opacity-40 text-lg text-white duration-300 hover:opacity-100'
-                onClick={scrollTop}
-            >
-                <HiArrowUp />
-            </button>
-        )}
-    </div>
-);
+            {showButton && (
+                <button
+                    className='p-1.5 fixed right-6 bottom-20 bg-gray-500 rounded-lg opacity-40 text-lg text-white duration-300 hover:opacity-100'
+                    onClick={scrollTop}
+                >
+                    <HiArrowUp />
+                </button>
+            )}
+        </div>
+    );
 }
